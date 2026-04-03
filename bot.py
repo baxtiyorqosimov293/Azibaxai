fcff406f981dd396645ecaf8aeeec88ab2dddcfe
baxtiyorqosimov293 <baxtiyorqosimov293@gmail.com>
2026-04-04 02:03:08 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -675,3 +675,18 @@ async def generate_handler(message: types.Message):
 if __name__ == "__main__":
     init_db()
     executor.start_polling(dp, skip_updates=True)
+import os
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+def run():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run).start()
